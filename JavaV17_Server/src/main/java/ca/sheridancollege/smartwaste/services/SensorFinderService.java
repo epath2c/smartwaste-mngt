ed8ac4e5209918084d10e5c13c0a9d09e32403b5
Jessiepxd <chenjiesunny@gmail.com>
2025-04-04 17:22:06 -0400

    comment out the whole SensorFinderService file, don't need it

--- JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/services/SensorFinderService.java
+++ JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/services/SensorFinderService.java
@@ -1,27 +1,27 @@
-package ca.sheridancollege.smartwaste.services;
-
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import ca.sheridancollege.smartwaste.beans.Sensor;
-import ca.sheridancollege.smartwaste.repositories.SensorRepository;
-import lombok.AllArgsConstructor;
-
-@Service
-@AllArgsConstructor
-public class SensorFinderService {
-
-    private SensorRepository sensorRepository;
-    
-    public Sensor findByMacAddressAndPins(String macAddress, int trigerPin, int echoPin) {
-        List<Sensor> sensors = sensorRepository.findAll();
-        
-        return sensors.stream()
-                .filter(s -> s.getMacAddress().equals(macAddress) && 
-                         s.getTrigerPin() == trigerPin && 
-                         s.getEchoPin() == echoPin)
-                .findFirst()
-                .orElse(null);
-    }
-}
+//package ca.sheridancollege.smartwaste.services;
+//
+//import java.util.List;
+//
+//import org.springframework.stereotype.Service;
+//
+//import ca.sheridancollege.smartwaste.beans.Sensor;
+//import ca.sheridancollege.smartwaste.repositories.SensorRepository;
+//import lombok.AllArgsConstructor;
+//
+//@Service
+//@AllArgsConstructor
+//public class SensorFinderService {
+//
+//    private SensorRepository sensorRepository;
+//    
+//    public Sensor findByMacAddressAndPins(String macAddress, int trigerPin, int echoPin) {
+//        List<Sensor> sensors = sensorRepository.findAll();
+//        
+//        return sensors.stream()
+//                .filter(s -> s.getMacAddress().equals(macAddress) && 
+//                         s.getTrigerPin() == trigerPin && 
+//                         s.getEchoPin() == echoPin)
+//                .findFirst()
+//                .orElse(null);
+//    }
+//}
