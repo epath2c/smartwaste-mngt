2b644e781ab31a7da72803b2632a830dc89fabcd
Sichao Quan <quansi@sheridancollege.ca>
2025-09-04 15:50:25 -0400

    add the home page

--- JavaV17_Server/src/main/webapp/src/app/app.routes.ts
+++ JavaV17_Server/src/main/webapp/src/app/app.routes.ts
@@ -8,17 +8,19 @@ import { CleanersComponent } from './cleaners/cleaners.component';
 import { TrashbinsListComponent } from './trashbins/trashbins-list/trashbins-list.component';
 import { TrashbinsAddComponent } from './trashbins/trashbins-add/trashbins-add.component';
 import { TrashbinsComponent } from './trashbins/trashbins.component';
-export const routes: Routes = [
+import { HomeComponent } from './home/home.component';
 
-    {'path':'sensors', component:SensorsComponent},
-    {'path':'cleaners', component:CleanersComponent},
-    {'path':'trashbins', component:TrashbinsComponent},
-    {'path':'view/sensors', component:SensorListComponent},
-    { 'path' :'view/sensors/:id/history', component: SensorHistoryComponent },
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, // Default route
+    { path: 'home', component: HomeComponent },
+    { path: 'sensors', component: SensorsComponent },
+    { path: 'cleaners', component: CleanersComponent },
+    { path: 'trashbins', component: TrashbinsComponent },
+    { path: 'view/sensors', component: SensorListComponent },
+    { path: 'view/sensors/:id/history', component: SensorHistoryComponent },
     { path: 'view/cleaners', component: CleanerListComponent },
     { path: 'add/cleaners', component: CleanerAddComponent },
     { path: 'view/trashbins', component: TrashbinsListComponent },
     { path: 'add/trashbins', component: TrashbinsAddComponent },
-
-
+    { path: '**', redirectTo: '' } // Catch all route
 ];
