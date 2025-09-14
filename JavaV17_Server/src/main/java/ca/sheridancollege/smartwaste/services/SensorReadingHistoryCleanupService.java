e909cbe4575e445e8c59a2d922fb4bede1693809
Shuya Hu <hushu@sheridancollege.ca>
2025-09-13 22:21:32 -0400

    finished auto clean old data feature

--- JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/services/SensorReadingHistoryCleanupService.java
+++ JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/services/SensorReadingHistoryCleanupService.java
@@ -1,5 +1,8 @@
 package ca.sheridancollege.smartwaste.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface SensorReadingHistoryCleanupService {
 	public void cleanOldData();
 }
--- JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/services/SensorReadingHistoryCleanupServiceImpl.java
+++ JavaV17_Server/src/main/java/ca/sheridancollege/smartwaste/services/SensorReadingHistoryCleanupServiceImpl.java
@@ -3,19 +3,22 @@ package ca.sheridancollege.smartwaste.services;
 import java.time.LocalDateTime;
 
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import ca.sheridancollege.smartwaste.repositories.SensorReadingHistoryRepository;
-
+import lombok.AllArgsConstructor;
+@Service
+@AllArgsConstructor
 public class SensorReadingHistoryCleanupServiceImpl implements SensorReadingHistoryCleanupService {
 	private SensorReadingHistoryRepository sensorReadingHistoryRepository;
 	
 	@Override
 	// Run daily at 2:00 AM
-    @Scheduled(cron = "0 0 2 * * ?")
+	@Scheduled(cron = "0 0 2 * * ?")
 	public void cleanOldData() {
 		LocalDateTime cutoff = LocalDateTime.now().minusDays(7);
         long deletedRows = sensorReadingHistoryRepository.deleteByTimestampBefore(cutoff);
-        System.out.println("Deleted {} sensor_reading_history rows older than {}" + deletedRows);
+        System.out.println("Deleted " + deletedRows + " sensor_reading_history rows older than 7 days");
 	}
 
 }
