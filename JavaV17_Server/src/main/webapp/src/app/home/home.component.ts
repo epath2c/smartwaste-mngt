064101029098703bc949a802f2b125f274dc8a89
Shuya Hu <hushu@sheridancollege.ca>
2025-11-16 09:50:31 -0500

    Display different components based on login status on home page

--- JavaV17_Server/src/main/webapp/src/app/home/home.component.ts
+++ JavaV17_Server/src/main/webapp/src/app/home/home.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../authentication/auth.service';
 import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
   title = 'Smart Waste Management System';
+  isLoggedIn$: Observable<boolean>;
+  constructor(private authService: AuthService) {
+    this.isLoggedIn$ = this.authService.isLoggedIn();
+  }
 }
